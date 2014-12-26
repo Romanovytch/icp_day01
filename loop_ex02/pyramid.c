27e58e6cc9039bc0566ba77dad2f45760fc124f0
alban meurice <alban.meurice@epitech.eu>
2014-12-26 22:52:48 +0100

    lapin des neiges

--- loop_ex02/pyramid.c
+++ loop_ex02/pyramid.c
@@ -1,11 +1,38 @@
-#include "brightcode.c"
+#include "brightcode.h"
 
-void	star_pyramid(unsignedint nb)
+void	star_pyramid(unsigned int nb)
 {
   int	tmp;
-  int	tmp
-
-  nb = (nb * 2) - 1;
-  tmp = (nb - 1) / 2;
-  while (tmp 
+  int	tmp2;
+  int	a;
+  int	z;
+  int	i;
+  
+  if (nb == 0)
+    bc_write_char('\n');
+  else
+    {
+      tmp2 = 2;
+      z = 1;
+      tmp = (nb * 2) - 1;
+      while (tmp2 != 0)
+	{
+	  i = 0;
+	  tmp2 = (tmp - z) / 2;
+	  a = tmp - (tmp - z);
+	  while (i != tmp2)
+	    {
+	      bc_write_char(' ');
+	      i = i + 1;
+	    }
+	  i = 0;
+	  while (i < a)
+	    {
+	      bc_write_char('*');
+	      i = i + 1;
+	    }
+	  bc_write_char('\n');
+	  z = z + 2;
+	}
+    }
 }
