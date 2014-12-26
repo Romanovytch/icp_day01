667d53ec45254d8669bae8bf5907b9cfef5c8808
alban meurice <alban.meurice@epitech.eu>
2014-12-26 20:55:27 +0100

    lol

---
+++ var_ex02/add.c
@@ -0,0 +1,23 @@
+int	addition(int a, int b)
+{
+  int	result;
+
+  result = a + b;
+  return (result);
+}
+
+int	substraction(int a, int b)
+{
+  int	result;
+
+  result = a - b;
+  return (result);
+}
+
+int	multiplication(int a, int b)
+{
+  int	result;
+
+  result = a * b;
+  return (result);
+}
